20cc30cba3dfb0664c0379bb527439280828f964
antoniopablodavid12-collab <antoniopablodavid12@gmail.com>
2026-05-24 05:18:17 +0800

    Create index.js

---
+++ index.js
@@ -0,0 +1,23 @@
+```js id="o6v2kd"
+const { Client, GatewayIntentBits } = require('discord.js');
+
+const client = new Client({
+  intents: [
+    GatewayIntentBits.Guilds,
+    GatewayIntentBits.GuildMessages,
+    GatewayIntentBits.MessageContent
+  ]
+});
+
+client.once('ready', () => {
+  console.log(`Logged in as ${client.user.tag}`);
+});
+
+client.on('messageCreate', message => {
+  if (message.content === '!ping') {
+    message.reply('Pong!');
+  }
+});
+
+client.login(process.env.TOKEN);
+```
